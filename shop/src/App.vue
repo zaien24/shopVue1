381d927e80c5be7bdc73594fb1b3a404b48f6c8b
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2021-06-12 07:27:36 +0900

    레이아웃 구성 및 시스템 오류 처리

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -1,4 +1,16 @@
 <template>
   <div id="app">
+    <Header />
+    <footer />
   </div>
 </template>
+<script>
+  import Header from  '@/components/Header.vue';
+  import Footer from  '@/components/Footer.vue';
+  export default {
+    components: {
+      Header,
+      Footer
+    }
+  }
+</script>
