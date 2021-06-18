e48a6de0b414c34e68fd9d68b95bf0bf7bd1aa5b
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2021-06-19 05:57:15 +0900

    메인페이지의 featured product 영역 구현2

--- shop/src/components/Header.vue
+++ shop/src/components/Header.vue
@@ -84,6 +84,7 @@
             <!-- Header cart noti -->
             <div class="header-cart header-dropdown">
               <ul class="header-cart-wrapitem">
+
                 <li class="header-cart-item">
                   <div class="header-cart-item-img">
                     <img src="images/item-cart-01.jpg" alt="IMG">
@@ -96,38 +97,6 @@
 
                     <span class="header-cart-item-info">
 											1 x $19.00
-										</span>
-                  </div>
-                </li>
-
-                <li class="header-cart-item">
-                  <div class="header-cart-item-img">
-                    <img src="images/item-cart-02.jpg" alt="IMG">
-                  </div>
-
-                  <div class="header-cart-item-txt">
-                    <a href="#" class="header-cart-item-name">
-                      Converse All Star Hi Black Canvas
-                    </a>
-
-                    <span class="header-cart-item-info">
-											1 x $39.00
-										</span>
-                  </div>
-                </li>
-
-                <li class="header-cart-item">
-                  <div class="header-cart-item-img">
-                    <img src="images/item-cart-03.jpg" alt="IMG">
-                  </div>
-
-                  <div class="header-cart-item-txt">
-                    <a href="#" class="header-cart-item-name">
-                      Nixon Porter Leather Watch In Tan
-                    </a>
-
-                    <span class="header-cart-item-info">
-											1 x $17.00
 										</span>
                   </div>
                 </li>
@@ -337,7 +306,14 @@
 </template>
 
 <script>
+  import { mapState, mapGetters } from 'vuex'
   export default {
+    ...mapState('cart', {
+      cartItems: state => state.items
+    }),
+    ...mapGetters('cart', {
+      totalCartPrice: 'totalPrice'
+    })
 
   }
 </script>
