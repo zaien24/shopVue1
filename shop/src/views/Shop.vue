052d50a3dc7ca0f38db9e044fa2970eb3039e902
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2021-06-19 07:17:48 +0900

    shop페이지 구현  - 상단영역 구현

--- shop/src/views/Shop.vue
+++ shop/src/views/Shop.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    shop page
+    <section class="bg-title-page p-t-50 p-b-40 flex-col-c-m" :style="`background-image: url(https://picsum.photos/1920/239/?image=526);`">
+      <h2 class="l-text2 t-center">
+        Women
+      </h2>
+      <p class="m-text13 t-center">
+        New Arrivals Women Collection 2018
+      </p>
+    </section>
   </div>
 </template>
 
