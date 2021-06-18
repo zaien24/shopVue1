7f58f34897f65c12bc44fa53f21585cf342bf78b
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2021-06-19 07:04:57 +0900

    장바구니 카운팅 구현

--- shop/src/components/Header.vue
+++ shop/src/components/Header.vue
@@ -79,7 +79,7 @@
 
           <div class="header-wrapicon2">
             <img src="images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="ICON">
-            <span class="header-icons-noti">0</span>
+            <span class="header-icons-noti">{{ cartItems.length }}</span>
 
             <!-- Header cart noti -->
             <div class="header-cart header-dropdown">
@@ -148,7 +148,7 @@
 
           <div class="header-wrapicon2">
             <img src="images/icons/icon-header-02.png" class="header-icon1 js-show-header-dropdown" alt="ICON">
-            <span class="header-icons-noti">0</span>
+            <span class="header-icons-noti">{{ cartItems.length }}</span>
 
             <!-- Header cart noti -->
             <div class="header-cart header-dropdown">
