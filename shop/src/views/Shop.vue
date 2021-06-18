7f9e2442214d82867d792a32ce3babb2381fd9c9
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2021-06-19 07:27:31 +0900

    shop페이지 구현  - priceFilter 컴포넌트 분리

--- shop/src/views/Shop.vue
+++ shop/src/views/Shop.vue
@@ -57,28 +57,7 @@
                 Filters
               </h4>
 
-              <div class="filter-price p-t-22 p-b-50 bo3">
-                <div class="m-text15 p-b-17">
-                  Price
-                </div>
-
-                <div class="wra-filter-bar">
-                  <div id="filter-bar"></div>
-                </div>
-
-                <div class="flex-sb-m flex-w p-t-16">
-                  <div class="w-size11">
-                    <!-- Button -->
-                    <button class="flex-c-m size4 bg7 bo-rad-15 hov1 s-text14 trans-0-4">
-                      Filter
-                    </button>
-                  </div>
-
-                  <div class="s-text3 p-t-10 p-b-10">
-                    Range: $<span id="value-lower">610</span> - $<span id="value-upper">980</span>
-                  </div>
-                </div>
-              </div>
+              <PriceFilter />
 
               <div class="filter-color p-t-22 p-b-50 bo3">
                 <div class="m-text15 p-b-12">
@@ -584,7 +563,10 @@
 </template>
 
 <script>
-
+  import PriceFilter from '@/components/shop/PriceFilter.vue'
   export default {
+    components: {
+      PriceFilter
+    }
   };
 </script>
